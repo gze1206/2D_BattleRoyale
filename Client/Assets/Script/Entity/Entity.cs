cb98a231f2dd0e06818a707967d0a365416ba351
박동욱 <phk09242@gmail.com>
2018-06-23 00:37:08 +0900

    플레이어 일부 제작

---
+++ Client/Assets/Script/Entity/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int HP;
+    public bool DeadObj;
+
+    public virtual void Dameged(int damege)
+    {
+        if(DeadObj)
+            HP -= damege;
+    }
+}
---
+++ Client/Assets/Script/Entity/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    // 착용 중인 무기
+    public GameObject Weapon;
+
+    private void Update()
+    {
+    }
+}
